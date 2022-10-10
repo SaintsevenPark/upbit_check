f48e032d95b35c69b69ea9a6b5ced567170c0852
SaintsevenParkHomeDesktop <ujinstar@gmail.com>
2022-10-10 13:05:38 +0900

    second commit at home

--- upbitcheck01.py
+++ upbitcheck01.py
@@ -10,23 +10,33 @@ st.set_page_config(
     layout="wide"
 )
 
-coin_list = pyupbit.get_tickers(fiat="KRW")
+
+@st.cache
+def get_coin_list():
+    rtn_coin_list = pyupbit.get_tickers(fiat="KRW")
+    return rtn_coin_list
 
 
-def coin_check(intervaltime, strategy):
+def coin_check(interval_time):
     st.markdown("#### 업비트 코인 체크")
     numbers = st.empty()
     with numbers.container():
-        for i in coin_list:
-            st.write(f"{i} : {intervaltime}")
-            ticker, interval, df = sta.getDf_name(i, intervaltime)
+        for i in get_coin_list():
+            st.write(f"{i} : {interval_time}")
+            ticker, interval, df = sta.getDf_name(i, interval_time)
             st.dataframe(df.tail(1))
             time.sleep(0.5)
 
 
-intervalTime = st.selectbox('기간 선택', ('minute5', 'minute10', 'minute15', 'minute30', 'minute60', 'D'))
-strategy = st.selectbox('전략 선택',
+coin_list = get_coin_list()
+coin_number = st.sidebar.number_input('코인넘버', min_value=0, max_value=len(coin_list), step=1)
+selected_coin = st.sidebar.selectbox('코인넘버', coin_list, index=coin_number)
+intervalTime = st.sidebar.selectbox('기간 선택', ('minute5', 'minute10', 'minute15', 'minute30', 'minute60', 'D'))
+strategy = st.sidebar.selectbox('전략 선택',
                         ('단순이평선교차',
                          'RSI'))
-if st.button("시작"):
-    coin_check(intervaltime=intervalTime, strategy=strategy)
+
+if st.sidebar.button("시작", disabled=True):
+    coin_check(interval_time=intervalTime)
+
+
